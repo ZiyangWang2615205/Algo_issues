d2ff0c5d154e053fa30e4fea12103944a6f66d10
Ziyang Wang <rl24988@bristol.ac.uk>
2026-02-27 02:16:53 +0000

    finish list setup and basic method

---
+++ src/List/Linklist.java
@@ -0,0 +1,80 @@
+package List;
+
+public class Linklist {
+    Node head;
+
+    //constructor
+    public Linklist(Node head){
+        this.head = head;
+    }
+    public Linklist(){
+        this.head = null;
+    }
+
+    //Basic method
+
+    //print example : 1->2->3->null
+    public void print(){
+        if(head == null) return;
+        Node cur = head;
+        while(cur != null){
+            System.out.print(cur.data + "->");
+            cur = cur.nxt;
+        }
+        System.out.print("null");
+    }
+
+    //add
+    public void add(Node addNode){
+        if(head == null) head = addNode;
+        Node cur = head;
+        while(cur.nxt != null){
+            cur = cur.nxt;
+        }
+        cur.nxt = addNode;
+    }
+
+    //remove example : 1->2->3->2, enter 2, list become 1->3
+    public void remove(int data){
+        if(head == null) return;
+        //if head should be removed
+        while(head.data == data){
+            head = head.nxt;
+        }
+        Node cur = head;
+        while(cur.nxt != null){
+            //1->2->3->2 => 1->3->2
+            if(cur.nxt.data == data){
+                cur.nxt = cur.nxt.nxt;
+            }else{
+                //avoiding null pointer
+                cur = cur.nxt;
+            }
+        }
+    }
+
+    //reverse example : 1->2->3->null => 3->2->1->null
+    public void reverse(){
+       if(head == null || head.nxt == null) return;
+       Node cur = head;
+       Node prev = null;
+       while(cur != null){
+           Node next = cur.nxt;
+           cur.nxt = prev;
+           prev = cur;
+           cur = next;
+       }
+       head = prev;
+    }
+
+    public static void main(String[] args) {
+        Linklist list = new Linklist(new Node(1));
+        list.add(new Node(2));
+        list.add(new Node(3));
+        list.add(new Node(2));
+        list.print();
+        list.reverse();
+        System.out.println();
+        list.print();
+    }
+}
---
+++ src/List/Node.java
@@ -0,0 +1,12 @@
+package List;
+
+public class Node {
+    int data;
+    Node nxt;
+
+    //constructor
+    public Node(int data){
+        this.data = data;
+        this.nxt = null;
+    }
+}
