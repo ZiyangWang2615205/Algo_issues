4db2ad28c013c1d2a885db0cc6f97d77fdec8c02
Ziyang Wang <rl24988@bristol.ac.uk>
2026-03-04 21:55:48 +0000

    finish fliptree

--- src/Tree/Node.java
+++ src/Tree/Node.java
@@ -185,13 +185,25 @@ public class Node {
         if(root.left == null && root.right == null) return true;
         return symmetricHelper(root.left,root.right);
     }
+
+    //flipTree:
+    //Example:	1					1
+    //
+    //		2		3	=>	    3		2
+    public static Node flipTree(Node root){
+        if(root == null) return null;
+        if(root.left == null && root.right == null) return root;
+        Node ansLeft = flipTree(root.right);
+        Node ansRight = flipTree(root.left);
+        root.left = ansLeft;
+        root.right = ansRight;
+        return root;
+    }
     public static void main(String[] args) {
-        Node root = new Node(1);
-        root.left = new Node(2);
-        root.left.right = new Node(4);
-        root.right = new Node(2);
-        root.right.left = new Node(4);
-        if(isSymmetric(root)) System.out.println("true");
+        Node root = new Node(2);
+        root.left = new Node(1);
+        root.right = new Node(3);
+        System.out.println(stackInorder(flipTree(root)));
     }
 
 }
