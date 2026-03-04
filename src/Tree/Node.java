d362c32823478e16018755b1188d3c6585706c8e
Ziyang Wang <rl24988@bristol.ac.uk>
2026-03-04 00:18:31 +0000

    finish preorder

--- src/Tree/Node.java
+++ src/Tree/Node.java
@@ -13,5 +13,22 @@ public class Node {
 
     public Node(){}
 
+    //order method:
+    //preorder: root-left-right
+    public static void preorder(Node root){
+        if(root != null){
+            System.out.print(root.data + "");
+            preorder(root.left);
+            preorder(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        preorder(root);
+    }
+
 
 }
