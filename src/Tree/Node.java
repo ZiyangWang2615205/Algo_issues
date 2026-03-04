7786326fd5d3b6164b6e1c2013ee6ff5298ab53e
Ziyang Wang <rl24988@bristol.ac.uk>
2026-03-04 00:19:52 +0000

    finish inorder

--- src/Tree/Node.java
+++ src/Tree/Node.java
@@ -17,17 +17,27 @@ public class Node {
     //preorder: root-left-right
     public static void preorder(Node root){
         if(root != null){
-            System.out.print(root.data + "");
+            System.out.print(root.data);
             preorder(root.left);
             preorder(root.right);
         }
     }
 
+    //inorder: left-root-right
+    public static void inorder(Node root){
+        if(root != null){
+            inorder(root.left);
+            System.out.print(root.data);
+            inorder(root.right);
+        }
+    }
+
+
     public static void main(String[] args) {
         Node root = new Node(1);
         root.left = new Node(2);
         root.right = new Node(3);
-        preorder(root);
+        inorder(root);
     }
 
 
