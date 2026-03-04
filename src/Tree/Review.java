092d56db0b4714b3b0d8cb5823570d1050665226
Ziyang Wang <rl24988@bristol.ac.uk>
2026-03-04 01:16:15 +0000

    create review of tree

---
+++ src/Tree/Review.java
@@ -0,0 +1,23 @@
+package Tree;
+
+import java.util.*;
+
+public class Review {
+    public static List<Integer> order(Node root){
+        if(root == null) return new ArrayList<>();
+        List<Integer> res = new ArrayList<>();
+        Stack<Node> stack = new Stack<>();
+        Node cur = root;
+        while(cur != null || !stack.isEmpty()){
+            //push left
+            while(cur != null){
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            res.add(cur.data);
+            cur = cur.right;
+        }
+        return res;
+    }
+}
