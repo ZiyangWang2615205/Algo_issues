ba12c34c36e7365ce93d5646ed65a993f0f7af51
Ziyang Wang <rl24988@bristol.ac.uk>
2026-03-10 11:44:48 +0000

    finish first version review of insertion sort

--- src/Sort/Review.java
+++ src/Sort/Review.java
@@ -2,5 +2,25 @@ package Sort;
 
 public class Review {
     //insertion sort
+    public static int[] insertion_sort(int[] arr){
+        if(arr.length <= 1) return arr;
+        for (int i = 0; i < arr.length; i++) {
+            int prev = i-1;
+            int cur = arr[i];
+            while(prev >= 0 && cur <= arr[prev]){
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+            arr[prev+1] = cur;
+        }
+        return arr;
+    }
     //quick sort
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{8,6,5,4,3,8,7};
+        for(int i : insertion_sort(arr)){
+            System.out.print(i);
+        }
+    }
 }
