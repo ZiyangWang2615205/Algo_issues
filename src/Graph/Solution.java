1c1bf27729a08d6b1413e06045daba01e03fd99e
Ziyang Wang <rl24988@bristol.ac.uk>
2026-03-13 13:29:20 +0000

    finish search target

--- src/Graph/Solution.java
+++ src/Graph/Solution.java
@@ -39,9 +39,50 @@ public class Solution {
         return res;
     }
 
+    //searchTarget: search target in incremental matrix(right>left,bot>top)
+    public static boolean searchTarget(int[][] matrix, int target){
+        if(matrix.length == 0) return false;
+        //matrix info
+        int top = 0, left = 0;
+        int bot = matrix.length-1;
+        int right = matrix[0].length-1;
+
+        //search col
+        int low = top;
+        int high = bot;
+        while(low <= high){
+            int mid = (low+high)/2;
+            if(matrix[mid][0] == target) return true;
+            if(matrix[mid][0] < target){
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+        }
+        int col = high;
+
+        //search row
+        low = left;
+        high = right;
+        while(low <= high){
+            int mid = (low+high)/2;
+            if(matrix[col][mid] == target) return true;
+            if(matrix[col][mid] < target){
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
         int[][] matrix = new int[][]{{1,2,3},{8,9,4},{7,6,5}};
         //check clockwise
         System.out.println(spiralOrder(matrix));
+        //search target in incremental matrix
+        int[][] increment = new int[][]{{1,2,3},{4,5,6},{7,8,9}};
+        if(searchTarget(increment,6)){
+            System.out.println("true");
+        }
     }
 }
